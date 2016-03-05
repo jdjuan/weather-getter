ef070fd3cb1fbf8058d6d41f90a79f2e264ac044
Juan Herrera <david.juanherrera@gmail.com>
2016-03-05 10:26:38 -0500

    SweetAlert added and validation finished

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,56 +1,74 @@
 $(function(){
+	var query= {"appid":"effc0084ed304b81b81ed7c6987a7865"};
 	if (navigator.geolocation) {
-		navigator.geolocation.watchPosition(showPosition,showError);
+		navigator.geolocation.getCurrentPosition(getPosition,showError);
 	} else {
-		alert("Geolocation is not supported by this browser.")
+		sweetAlert("Oops...", "Geolocation is not supported by this browser!", "error");
 	}
 
-	function showPosition(position) {
-		var latitude = position.coords.latitude;
-		var longitude = position.coords.longitude
-		// var latlon = latitude + "," + longitude;
-		$.getJSON( "http://api.openweathermap.org/data/2.5/weather?",{
-			lat : latitude,
-			lon : longitude,
-			appid: "effc0084ed304b81b81ed7c6987a7865"
-		}, function( data ) {
-			var table = $("table.information");
-			var html = "";
-			html += "<tr><td>Temperature:</td><td>"+data.main.temp+"°F</td></tr>";
-			html += "<tr><td>Description:</td><td>"+data.weather[0].description+"</td></tr>";
-			html += "<tr><td>Humidity:</td><td>"+data.main.humidity+"%</td></tr>";
-			html += "<tr><td>Pressure:</td><td>"+data.main.pressure+" hpa</td></tr>";
-			$("#city").text(data.name);
-			table.html(html);	
-			// alert(data.coord.lon);
-		}).done(function() {
-			console.log( "second success" );
-		}).fail(function() {
-			console.log( "error" );
-		});
-
-		// var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false";
-		// message.html(latlon);
-		// message.html(message.html() + "<img src='"+img_url+"'>");
+	function getPosition(position) {
+		query["lat"]=position.coords.latitude;
+		query["lon"]=position.coords.longitude;
+		fetchData();
 	}
 
 	function showError(error) {
 		var errorMessage;
 		switch(error.code) {
 			case error.PERMISSION_DENIED:
-			errorMessage = "User denied the request for Geolocation."
+			errorMessage = "Oh!, Geolocation is not allowed"
 			break;
 			case error.POSITION_UNAVAILABLE:
-			errorMessage = "Location information is unavailable."
+			errorMessage = "Oh!, Geolocation is not available."
 			break;
 			case error.TIMEOUT:
-			errorMessage= "The request to get user location timed out."
+			errorMessage= "Oh!, It took more than often"
 			break;
 			case error.UNKNOWN_ERROR:
-			errorMessage= "An unknown error occurred."
+			errorMessage= "Oh!, something very weird just happpened"
 			break;
 		}
-		alert(errorMessage);
+		swal({title: errorMessage,   
+			text: "No problem, we've got it, just type in your postal code or city:",   
+			type: "input",   
+			showCancelButton: false,   
+			closeOnConfirm: false,   
+			animation: "slide-from-top",   
+			inputPlaceholder: "London" }, function(inputValue){   
+				if (inputValue === false) return false;      
+				if (inputValue === "") {     
+					swal.showInputError("You are funny, just type in your city name ;)");     
+					return false   
+				}      
+				if ($.isNumeric(inputValue)) {
+					query["zip"]=inputValue;
+				}else{
+					query["q"]=inputValue;
+				}
+				fetchData();
+			});
+	}
+
+	function fetchData(){
+		$.getJSON( "http://api.openweathermap.org/data/2.5/weather?",query, function( data ) {
+			var table = $("table.information");
+			var html = "";
+			$("#city").text(data.name);
+			html += "<tr><td>Temperature:</td><td>"+data.main.temp+"°F</td></tr>";
+			html += "<tr><td>Description:</td><td>"+data.weather[0].description+"</td></tr>";
+			html += "<tr><td>Humidity:</td><td>"+data.main.humidity+"%</td></tr>";
+			html += "<tr><td>Pressure:</td><td>"+data.main.pressure+" hpa</td></tr>";
+			table.html(html);
+		}).done(function() {
+			swal({title:"We've made it!", 
+				text:"Here is your weather!", 
+				type: "success",
+				timer: 2000,
+				showConfirmButton: false
+			});
+		}).fail(function() {
+			sweetAlert("Oops...", "There was a problem with the server, try again later!", "error");
+		});
 	}
 });
 
